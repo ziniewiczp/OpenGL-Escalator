89903510a4df586a9fd7b8222b4845b0cc9897ab
rusty-2 <tmszraus@gmail.com>
2017-06-06 19:22:35 +0200

    zwieksz obszar po jakim porusza sie kamera

--- konfiguracja.cpp
+++ konfiguracja.cpp
@@ -12,7 +12,7 @@
 
 	// KONFIGURACJA	
 	
-	ustalObszar (150);						// promien obszaru (kola), po jakim kamera mo¿e sie poruszaæ
+	ustalObszar (250);						// promien obszaru (kola), po jakim kamera mo¿e sie poruszaæ
 
 	// Funkcja rejestruj¹ca przeszkody - kolejne parametry to X1, Z1, X2, Z2 prostok¹ta, który jest dodatkowo zabroniony dla kamery
 	// Funkcjê mo¿na wywo³ywac wielokrotnie dla ró¿nych obszarów
