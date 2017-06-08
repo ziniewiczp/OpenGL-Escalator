6a937437046979ea16b67781fbc2be72762ed2c3
rusty-2 <tmszraus@gmail.com>
2017-06-08 10:55:22 +0200

    change cogwheel appearance

--- rysowanie.cpp
+++ rysowanie.cpp
@@ -27,28 +27,29 @@
 
 	glTranslatef(17.5, 6, -25); // vector
 	glRotatef(-35, 0, 1, 0);
-
-	glPushMatrix();
-	glTranslatef(0,-4,-9.5);
-	glRotatef(90, 0, 0, 1);
-	glScalef(0.15,0.15, 0.15);
-	if (rotate == 360)
-		rotate = 0;
-	glRotatef(rotate, 0, 1, 0);
-	//rysujModel("cogwheel.3ds");
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(10, 115, -210);
-	glRotatef(90, 0, 0, 1);
-	glScalef(0.15, 0.5, 0.15);
-	if (rotate == 360)
-		rotate = 0;
-	glRotatef(rotate, 0, 1, 0);
-	//rysujModel("cogwheel.3ds");
-	glPopMatrix();
-
-	rotate += 0.5;
+	for (int i = 0; i < 2; i++) {
+		glPushMatrix();
+		glTranslatef(-17+(i*35), -4, -12.5);
+		glRotatef(90, 0, 0, 1);
+		glScalef(0.15, 0.1, 0.30);
+		if (rotate == 360)
+			rotate = 0;
+		glRotatef(rotate, 0, 1, 0);
+		rysujModel("cogwheel.3ds");
+		glPopMatrix();
+	}
+	for (int i = 0; i < 2; i++) {
+		glPushMatrix();
+		glTranslatef(-17+(i*35), 115, -210);
+		glRotatef(90, 0, 0, 1);
+		glScalef(0.15, 0.1, 0.30);
+		if (rotate == 360)
+			rotate = 0;
+		glRotatef(rotate, 0, 1, 0);
+		rysujModel("cogwheel.3ds");
+		glPopMatrix();
+	}
+	rotate += 0.4;
 
 	for (int i = 0; i < stairsCount; i++)
 	{
